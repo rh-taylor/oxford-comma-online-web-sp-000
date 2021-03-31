0aedfd0cbefd39e6472280cff6759c29865a87a1
Robyn Taylor <rhtaylor0715@gmail.com>
2021-03-31 00:47:09 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return "#{array[0]}"
+  elsif array.length == 2
+    return array.join(" and ")
+  elsif array.length >= 3
+    and_elem = "and #{array[-1]}"
+    array.pop
+    array << and_elem
+    array.join(", ")
+  end
 end
