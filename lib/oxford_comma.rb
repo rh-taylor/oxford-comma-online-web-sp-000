49c8ceb9e9f36d9fe48e9fd30f68922a9c4ac322
Robyn Taylor <rhtaylor0715@gmail.com>
2021-03-31 00:16:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,6 @@ def oxford_comma(array)
   elsif array.length == 2
     return array.join(" and ")
   elsif array.length > 3
-    array.pop
     array.join(", ")
     array << "and #{array[-1]}"
   end
